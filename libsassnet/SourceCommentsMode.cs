0e4b93b0dd69f53799e086bd94d28e6585366d8f
Darren Kopp <darrenkopp@gmail.com>
2014-03-25 23:01:39 -0600

    missed file
    github didn't refresh i guess

---
+++ libsassnet/SourceCommentsMode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibSassNet
+{
+    public enum SourceCommentsMode : int
+    {
+        None = 0,
+        Default = 1,
+        SourceMaps = 2
+    }
+}
