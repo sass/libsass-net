dbaef56e7a508c99f03da1bc272e696971f65afb
Darren Kopp <darrenkopp@gmail.com>
2014-05-26 18:55:18 -0600

    fix all the broken things

--- publish.ps1
+++ publish.ps1
@@ -3,9 +3,12 @@
 [string]$token = Read-Host -Prompt "What is the token";
 $token = $token.Trim();
 
+[string]$version = Read-Host -Prompt "What version";
+$version = $version.Trim();
+
 
 $packages = @("libsassnet", "libsassnet.Web");
 $packages | %{ 
-    $packageName = $_ + ".nupkg";
-    & '.\.nuget\NuGet.exe' $packageName $token
+    $packageName = $_ + "." + $version + ".nupkg";
+    .\.nuget\NuGet.exe Push $packageName $token;
 };
